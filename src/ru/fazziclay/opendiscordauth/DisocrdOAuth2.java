c7b05699be1571d38462a62bd9c86fc6c10b9daf
TheKing-OfTime <artem.matvienko0@gmail.com>
2023-07-21 15:20:12 +0300

    Fix P0 bug when bot triggers to itself

---
+++ src/ru/fazziclay/opendiscordauth/DisocrdOAuth2.java
@@ -0,0 +1,9 @@
+package ru.fazziclay.opendiscordauth;
+
+import org.apache.hc.client5.http.fluent.Request;
+import org.apache.hc.client5.http.fluent.Content;
+import org.apache.hc.client5.http.fluent.Form;
+
+public class DisocrdOAuth2 {
+    private static final String BASE_URL = "";
+}
